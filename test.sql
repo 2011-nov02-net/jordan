ca7d6808834f16259ae782f2b01af529b2a06a22
Jordan Garcia <36669866+JordanEGarcia@users.noreply.github.com>
2020-11-16 09:46:09 -0600

    Add files via upload

--- test.sql
+++ test.sql
@@ -32,14 +32,29 @@ VALUES
 ('Jordan', 'Garcia', 'jg@gmail.com', '888123123455'),
 ('Isidro', 'Buenro', 'Ib@yahoo.com', '342526234455'),
 ('Gabby', 'McDude', 'gb@yahoo.com', '889219454555'),
-('Tina', 'Smith', 'TS@crouton.net', '345819534755'),
+('Tina', 'Smith', 'TS@crouton.net', '345819534755'), -- inserting tina smith
 
 
 INSERT Into Product (Name, PRICE)
 VALUES 
-("IPAD", 300)
-("IPHONE", 200)
-("IPOD", 100)
-("NANO", 50)
+("IPAD", 300),
+("IPHONE", 200),
+("IPOD", 100),
+("NANO", 50);
 
-INSERT INTO Order
+INSERT INTO Order (ProductId, CustomerId)
+VALUES 
+(4,2), --Tina buying an iphone
+(4,1),
+(1,4);
+
+Select * From Order -- display all orders
+Where OrderId=104;
+
+Select SUM(PRICE) From Order
+Inner Join Product on Product.OrderId = Order.OrderId
+where ProductId = 4
+
+Update Product
+Set PRICE=250
+where ProductId=2;
